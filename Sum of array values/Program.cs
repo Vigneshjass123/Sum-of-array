a56d91f9f7706ede1f57fdf4ae833baf5b73192f
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-08 23:53:13 +0530

    Add project files.

---
+++ Sum of array values/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_array_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 6, 8, 10 };
+            int sum = 0;
+            for(int i = 0;i< arr.Length;i++)
+                {
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("sum of array value is= " +  sum);
+        }
+    }
+}
